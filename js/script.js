b7ec386c35a057734944926aaa4347e9766cdd9e
Данил Дятьков <dyatkov2023@spobip-bel.ru>
2025-03-24 08:12:38 +0300

    Site done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const menuItems = document.querySelector('.menu-items');
 
-hamburgerIcon.addEventListener('click', function () {
-    menuItems.classList.toggle('active');
+function closeMenu() {
+    hamburgerIcon.classList.remove('open');
+    menuItems.classList.remove('open');
+  }
+
+hamburgerIcon.addEventListener('click', () => {
+  hamburgerIcon.classList.toggle('open');
+  menuItems.classList.toggle('open');
 });
+
+document.addEventListener('click', (event) => {
+    const isClickInsideMenu = menuItems.contains(event.target);
+    const isClickInsideHamburger = hamburgerIcon.contains(event.target);
+  
+    if (!isClickInsideMenu && !isClickInsideHamburger && menuItems.classList.contains('open')) {
+      closeMenu();
+    }
+  });
