90e2769df47709950042087e67585fc9c3091ab4
VinCat01 <Vindov.cat@gmail.com>
2025-04-04 10:07:43 +0300

    Слайдер на вторую страницу progress 35%

---
+++ index.js
@@ -0,0 +1,10 @@
+
+new Swiper('.swiper', {
+    loop:true,
+    slidesPerViev: 1,
+    spaceBetween:20,
+    navigation:{
+        prevEl: '.card__btn--prev',
+        nextEl: '.card__btn--next',
+    }
+});
